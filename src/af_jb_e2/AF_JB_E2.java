cf2f89a9816826f40900d7ec4ebf85b792ec965c
Edwar <EdwarUlianov@users.noreply.github.com>
2017-07-31 00:20:59 +0200

    Comentario mejorado

--- src/af_jb_e2/AF_JB_E2.java
+++ src/af_jb_e2/AF_JB_E2.java
@@ -9,7 +9,7 @@ public class AF_JB_E2 {
     //Método para contar de 0 a un número dado por el usuario
     static void MostrarNumero(){
         
-        //Inicializo un objeto del tipo Scanner y declaro dos variables tipo 
+        //Inicializo un objeto de la clase Scanner y declaro dos variables tipo 
         //entero
         Scanner leer = new Scanner(System.in);
         int n;
