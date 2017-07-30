0f3ebf390daade4adfb380ceda6bff5611307c41
Edwar <Edwar@Blackstar>
2017-07-31 00:13:21 +0200

    Creo el método Edad y lo llamo en main para ver si funciona.

--- src/af_jb_e2/AF_JB_E2.java
+++ src/af_jb_e2/AF_JB_E2.java
@@ -24,15 +24,40 @@ public class AF_JB_E2 {
         for (i=0; i<=n; i++){
             System.out.println(i);
         }
-                            
+    }
+        
+    //Creo el método Edad que me dirá si el usuario es mayor o menor de edad
+    static void Edad(){
+       
+//Inicializo un objeto del tipo Scanner
+       Scanner leer = new Scanner(System.in);
+       int n;
+       
+       //Pido la edad al usuario y la almeceno en n
+       System.out.println("Introduzca su edad");
+       n = leer.nextInt();
+       
+       //Compruebo si es menor de edad y se lo comunico usando para ello el
+       //condicional if
+       if (n<18){
+           System.out.println("Eres menos de edad");
+       }
+       else {
+           System.out.println("Es usted mayor de edad");
+       }
+               
         
     }
+    
+                            
+        
+    
 
     public static void main(String[] args) {
         
         //Lamo al método MostrarNumero
         MostrarNumero();
-
+        Edad();
 
     }
     
